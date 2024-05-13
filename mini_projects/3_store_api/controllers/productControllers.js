275caf0e21459f7d113e506f2e92d20b99759279
solARis <nitishraosnr2001@gmail.com>
2024-05-13 23:45:22 +0530

    added searching for numeric filters

--- mini_projects/3_store_api/controllers/productControllers.js
+++ mini_projects/3_store_api/controllers/productControllers.js
@@ -1,20 +1,38 @@
 import { Product } from "../models/productModels.js"
 
 const getAllproductsStatic = async (req, res)=>{
-    const products = await Product.find({})
-    .sort('-price -name')
-    .select('name price')
-    .limit(2)
-    .skip(0)
+    const products = await Product.find({price : {$gte : 100}})
     res.json({products, nbHits : products.length});
 }
 
 const getAllproducts = async (req, res)=>{
-    const {featured, company, name, sort, fields} = req.query
+    const {featured, company, name, sort, fields, numericFilters} = req.query
     const queryObject = {}
     if(featured) queryObject.featured = featured==='true' ? true : false;
     if(company) queryObject.company = company;
     if(name) queryObject.name = {$regex : name, $options : "i"};
+    if(numericFilters)
+    {
+        const op = {
+            '<' : '$lt',
+            '<=' : '$lte',
+            '=' : '$eq',
+            '>' : '$gt',
+            '>=' : '$gte',
+        }
+        const regEx = /\b(<|<=|=|>=|>)\b/g
+        let filters = numericFilters.replace(regEx, match=>`-${op[match]}-`)
+        
+        const options = ['price', 'rating']
+        filters.split(',').forEach(element => {
+            const [field, operator, value] = element.split('-');
+            if(options.includes(field))
+            {
+                queryObject[field] = {[operator] : Number(value)}
+            }
+        });
+    }
+    
     let result = Product.find(queryObject)
     if(sort)
     {
